d3b96ef631da9c0be32aadce3789d9b00d284c9c
Tyler <tyler88128949@gmail.com>
2024-09-19 17:52:38 +0800

    feat: finish type.c

--- type.c
+++ type.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
+#define BOOL char
+#define FALSE 0
+#define TRUE 1
+
 int main()
 {
+
     int foo;
     int bar = 1;
     printf("%d %d\n", foo, bar);
 
     int a = 0, b = 1, c = 2, d = 3, e = 4;
     a = b - c + d * e;
-    printf("%d", a);
+    printf("%d\n", a);
+
+    BOOL true;
+    true = TRUE;
+    printf("%d", true);
     return 0;
 }
